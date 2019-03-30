30a98632899706484f795406e0f99f584d749a55
CodenameCOTC <a.novaladip@yahoo.com>
2019-03-30 18:39:44 +0700

    test dogs schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    // create a dog
+    return newDog;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
