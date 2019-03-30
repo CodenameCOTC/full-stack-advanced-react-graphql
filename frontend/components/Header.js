2852afcae9630951532d6c2358903e01b85d32be
CodenameCOTC <a.novaladip@yahoo.com>
2019-03-30 16:58:34 +0700

    added NProgress to Header component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import styled from "styled-components";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import Nav from "./Nav";
+
+Router.onRouteChangeStart = () => NProgress.start();
+
+Router.RouterChangeComplete = () => NProgress.done();
+
+Router.onRouteChangeError = () => NProgress.done();
 
 const Logo = styled.h1`
   font-size: 4rem;
